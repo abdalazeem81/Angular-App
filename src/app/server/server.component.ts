a62208982fcff67feb9d655725e7a614ac3782e9
Abdalazeem Ataby <abdalazeem.abdalhameed81@gmail.com>
2023-05-14 11:12:36 +0300

    ngIF

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,32 @@ templateUrl : "./server.component.html",
 styleUrls : ["./server.component.css"]
 })
 export class ServerComponent{
+ServerId: number = 10;
+ServerStatus: string = "OffLine";
+ServerMsg: string = "I am a Message";
+ServerCreationStatus = "No Server Was Created Yet.";
+allowNewServer: boolean = false;
+ServerName = "";
+IsServerCreated = false;
+GetServerMsg(){
+    return this.ServerMsg;
+}
+
+constructor(){
+    setTimeout(() => {
+        this.allowNewServer = true;
+    }, 2000);
+}
+
+// On Click Of Add New Server Button
+OnServerCreation() {
+    this.ServerCreationStatus = "Server Was Created Successfully!";
+    this.IsServerCreated = true;
+}
+
+// On Input Value Change
+OnServerNameChange(event:Event){
+    this.ServerName = (<HTMLInputElement>event.target).value;
+}
 
 }
