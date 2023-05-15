792dccf15634a19a9f1144bf59cdc1ad198b0cd2
Abdalazeem Ataby <abdalazeem.abdalhameed81@gmail.com>
2023-05-15 09:21:43 +0300

    Add Ingrdient model and form for ingredient

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+Recipes: Recipe[] = [
+  new Recipe('A Test Recipe', 'A Simple Description', 'https://img.taste.com.au/mOx3fOxf/w720-h480-cfill-q80/taste/2022/09/garlic-chilli-prawn-pasta-181440-1.jpg'),
+  new Recipe('Pizza', 'This description for pizza', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHs0yVWxEWiTLnwy4Ndg-XMi3uZVJ4fLu7ng&usqp=CAU'),
+  new Recipe('Chinese Food', 'Seafood description','https://thecozycook.com/wp-content/uploads/2022/04/Lasagna-Recipe-f.jpg')
+];
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Ingredient } from '../shared/Ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-
+ingredients: Ingredient[] = [
+  new Ingredient('Egg', 5),
+  new Ingredient('Tomatos', 3),
+  new Ingredient('Flower', 1),
+  new Ingredient('Milk', .5)
+];
 }
